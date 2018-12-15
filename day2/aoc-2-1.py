3fb858083662ad7e1763ce913b39a66e27ee6d76
Alexander Stathis <stathis.alexanderj@gmail.com>
2018-12-14 23:16:37 -0500

    part 1 solved

---
+++ day2/aoc-2-1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# this is a solution to part 1 of Advent of Code, day 2.
+
+f = open("input.txt", "r")
+lines = list(f)
+f.close()
+
+twos = 0
+threes = 0
+
+for l in lines:
+  line = list(l)
+  line.sort()
+  
+  print(line)
+  
+  i = 0
+  j = 1
+  
+  twice = False
+  thrice = False
+  
+  while j < len(line):
+    while j < len(line) and line[i] == line[j]:
+      j += 1
+    if j - i == 2 and not twice:
+      twos += 1
+      twice = True
+    elif j - i == 3 and not thrice:
+      threes += 1
+      thrice = True
+    i = j
+    j = i+1
+    
+print(twos * threes)
