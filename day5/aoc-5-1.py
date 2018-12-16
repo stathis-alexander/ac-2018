8393a88d443571396511083e51ff1ff7f10fab54
Alexander Stathis <stathis.alexanderj@gmail.com>
2018-12-16 17:20:27 -0500

    day 5 part 1 solved

--- day5/aoc-5-1.py
+++ day5/aoc-5-1.py
@@ -3,7 +3,19 @@
 
 f = open("input.txt","r")
 polymerstring = f.read()
+polymerstring = polymerstring[:-1]
 f.close()
 
-print(polymerstring)
+i = 0
+while i < len(polymerstring)-1:
+  char = polymerstring[i]
+  nextchar = polymerstring[i+1]
+  if (char.isupper() and char.lower() == nextchar) or (char.islower() and char.upper() == nextchar):
+    polymerstring = polymerstring[:i] + polymerstring[i+2:]
+    if i != 0:
+      i -= 1
+  else:
+    i += 1
 
+
+print(len(polymerstring))
