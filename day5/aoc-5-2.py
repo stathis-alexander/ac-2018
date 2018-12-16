e9fee0543d4fa938102c281c1d2c30c477a92c7d
Alexander Stathis <stathis.alexanderj@gmail.com>
2018-12-16 17:36:41 -0500

    day 5 part 2 solved

--- day5/aoc-5-2.py
+++ day5/aoc-5-2.py
@@ -2,20 +2,33 @@
 # This is a solution to part 2 of Advent of Code, day 5. 
 
 f = open("input.txt","r")
-polymerstring = f.read()
-polymerstring = polymerstring[:-1]
+polymerinput = f.read()
+polymerinput = polymerinput[:-1]
 f.close()
 
-i = 0
-while i < len(polymerstring)-1:
-  char = polymerstring[i]
-  nextchar = polymerstring[i+1]
-  if (char.isupper() and char.lower() == nextchar) or (char.islower() and char.upper() == nextchar):
-    polymerstring = polymerstring[:i] + polymerstring[i+2:]
-    if i != 0:
-      i -= 1
-  else:
-    i += 1
+def react(polymerstring):
+  i = 0
+  while i < len(polymerstring)-1:
+    char = polymerstring[i]
+    nextchar = polymerstring[i+1]
+    if (char.isupper() and char.lower() == nextchar) or (char.islower() and char.upper() == nextchar):
+      polymerstring = polymerstring[:i] + polymerstring[i+2:]
+      if i != 0:
+        i -= 1
+    else:
+      i += 1
+  return polymerstring
 
+shortestlength = -1
 
-print(len(polymerstring))
+for char in "abcdefghijklmnopqrstuvwxyz":
+  polymerstr = polymerinput[:]
+  polymerstr = polymerstr.replace(char,"")
+  polymerstr = polymerstr.replace(char.upper(),"")
+
+  editedpolymer = react(polymerstr)
+  
+  if (shortestlength == -1) or (len(editedpolymer) < shortestlength):
+    shortestlength = len(editedpolymer)
+
+print(shortestlength)
