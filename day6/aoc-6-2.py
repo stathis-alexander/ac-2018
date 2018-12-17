88f9ae2555f19123afe6e44d0a12aaa098c1b8e9
Alexander Stathis <stathis.alexanderj@gmail.com>
2018-12-17 13:00:33 -0500

    day 6 part 2 solved

--- day6/aoc-6-2.py
+++ day6/aoc-6-2.py
@@ -12,42 +12,21 @@ x_big = max(xes)
 y_small = min(yes)
 y_big = max(yes)
 
-def find_nearest(u,v):
-  disttonear = 1000
-  nearest = None
-  unique_nearest = True
+def compute_weight(u,v):
+  weight = 0
 
   for coord in coordlist:
-    distance = abs(coord[1] - v) + abs(coord[0] - u)
-    if distance < disttonear:
-      unique_nearest = True
-      nearest = coord
-      disttonear = distance
-    elif distance == disttonear:
-      unique_nearest = False
+    weight += abs(coord[1] - v) + abs(coord[0] - u)
   
-  return unique_nearest,nearest,disttonear
+  return weight
 
-def on_boundary(u,v):
-  if u in [x_small,x_big] or v in [y_small,y_big]:
-    return True
-  else:
-    return False
-
-infinite_set = set()
-areas = dict(zip(coordlist,[0]*len(coordlist)))
+good_set = set()
 
 for u in range(x_small,x_big+1):
   for v in range(y_small,y_big+1):
-    [unique,nearest,distance] = find_nearest(u,v)
-    
-    if not unique or nearest in infinite_set:
-      continue
-    elif on_boundary(u,v):
-      infinite_set.add(nearest)
-      areas[nearest] = 0
-    else:
-      areas[nearest] = areas[nearest] + 1
+    weight = compute_weight(u,v)
+    if weight < 10000:
+      good_set.add((u,v))    
 
-print(max([areas[item] for item in areas]))
 
+print(len(good_set))
