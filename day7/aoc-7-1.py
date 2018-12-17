6c4f03e0dc0d1b141036b4091d4e1c2e0eb1ebd9
Alexander Stathis <stathis.alexanderj@gmail.com>
2018-12-17 15:32:57 -0500

    day 7 part 1 solved

--- day7/aoc-7-1.py
+++ day7/aoc-7-1.py
@@ -1,9 +1,43 @@
 #!/usr/bin/python
 # This is a solution to Advent of Code, day 7 part 1. 
 
-
 with open("input.txt","r") as f:
-  lines = [line.rstrip("\n") for line in f.readlines()]
+  dependencies = [(line[5],line[36]) for line in f.readlines()]
 
-for line in lines:
-  print(line)
+letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+node_table = dict(zip(letters,[[] for x in range(26)]))
+
+for depends in dependencies:
+  first = depends[0]
+  second = depends[1]  
+
+  node_table[first].append(second)
+
+heads = []
+seen = []
+
+for letter in letters:
+  if(not any([letter in node_table[key] for key in node_table])):
+    heads.append(letter)
+
+for key in node_table:
+  print(key,":",node_table[key])
+
+while heads:
+  heads.sort(reverse=True)
+  head = heads.pop()
+
+  print(head,heads)
+
+  if head in seen:
+    continue
+  else:
+    seen.append(head)
+  for x in node_table[head]: 
+    if(not any([(key not in seen) and (x in node_table[key]) for key in node_table])):
+      heads.append(x)
+  
+  heads = list(set(heads))
+
+print(''.join(seen))
