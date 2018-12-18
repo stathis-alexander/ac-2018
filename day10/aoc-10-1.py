138f9232683490c307c8bd6a8ef0f9c775132793
Alexander Stathis <stathis.alexanderj@gmail.com>
2018-12-17 21:23:30 -0500

    setup day 10

---
+++ day10/aoc-10-1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# This is a solution to Advent of Code, day 10 part 1.
+
+with open("input.txt","r") as f:
+  lines = [line.rstrip("\n") for line in f.readlines()]
+
+for line in lines:
+  print(line)
